39bd84c9a98c804d3979535416392cc2bf738e3b
Sarveshwar <sarveshwar.scorpio@gmail.com>
2025-09-10 09:25:44 +0400

    added gitignore

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schoolsub.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route("/")
 def home():
     return {"message": "Backend is working!"}
